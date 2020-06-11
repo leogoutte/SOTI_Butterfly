d6642a80e44e588497b4c233c7ecb179e80ab028
leogoutte <leo.goutte@gmail.com>
2020-06-11 11:26:44 -0400

    Looked at PFT, P = 1/2. Made a few other minor changes.

--- soti_tools.py
+++ soti_tools.py
@@ -3,6 +3,8 @@
 import numpy as np
 from scipy.linalg import block_diag
 import matplotlib.pyplot as plt
+import scipy.sparse.linalg as spars
+
 
 # sigma matrices
 def sig(n):
@@ -63,88 +65,178 @@ def Harper_SOTI(p, q, mu = 0, nu = 0, zu = 0, M = 2.3, D1 = 0.8, D2 = 0.5):
     
     return Ha
 
+# imshow of matrix entries
+def matrix_form(p=1,q=10):
+    H = Harper_SOTI(p,q)
+    plt.figure(figsize=(10,10))
+    plt.title(r"SOTI Harper matrix for $q = {} $".format(q), fontsize = 20, **futura)
+    plt.imshow(np.abs(H), cmap = 'inferno')
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
 # eigenvalues of harper matrices
 def eigs_harper(H):
     # computes eigs of Harper matrix
     # returns list of eigs
+    # q = int(np.shape(H)[0])
+    # spars scales better for large q
+    eigs = np.linalg.eigvalsh(H) #, k = q, return_eigenvectors=False)
     
-    eigs = np.linalg.eigvalsh(H)
-    
-    return list(eigs
+    return list(eigs)
 
 # Greatest Common Divisor
 def gcd(a, b):
     if b == 0: 
-    	return a
-    return gcd(b, a % b)
+        return a
+    else:
+        return gcd(b, a % b)
 # if gcd == 1 then the fraction is irreducible
 
 # main function
 # makes phis and energies
 def main(qmax = 100, mu = 0, nu = 0, zu = 0, 
-	M = 2.3, D1 = 0.8, D2 = 0.5):
+    M = 2.3, D1 = 0.8, D2 = 0.5):
 
     phi = []
     eps = []
     for q in range(1, qmax):
         for p in range(0, q): # all p s.t p/q < 1
-            if gcd(p,q) == 1:
-                # add all possible phi (q copies of p/q and 1-p/q)
-                phi.extend([p/q]*4*q + [(q-p)/q]*4*q)
-                # compute eigs
-                Harper = Harper_SOTI(p, q, mu, nu, zu, M, D1, D2)
-                eigs_pq = eigs_harper(Harper)
-                # add each eig twice for same reason as above (hermicity) # <- this may not apply anymore
-                eps.extend(eigs_pq*2)
+            #if gcd(p,q) == 1:
+            # add all possible phi (q copies of p/q and 1-p/q)
+            phi.extend([p/q]*4*q + [(q-p)/q]*4*q)
+            # compute eigs
+            Harper = Harper_SOTI(p, q, mu, nu, zu, M, D1, D2)
+            eigs_pq = eigs_harper(Harper)
+            # add each eig twice for same reason as above (hermicity) # <- this may not apply anymore
+            eps.extend(eigs_pq*2)
                 
     # return phis and energies
     phi = np.asarray(phi)
     eps = np.asarray(eps)
-    return phi, 
+    return phi, eps
 
 # spectrum creator
 def spectrum(p, q, dimless_param, resolution = 100, 
-	unit_cell_range = True, unit_cell_scale = 1):
+    unit_cell_range = True, unit_cell_scale = 1):
 
-	if unit_cell_range == True:
-		ks = np.linspace(-np.pi/unit_cell_scale, np.pi/unit_cell_scale, resolution)
-	elif unit_cell_range == False:
-		ks = np.linspace(0, 2*np.pi/unit_cell_scale, resolution)
+    if unit_cell_range == True:
+        ks = np.linspace(-np.pi/unit_cell_scale, np.pi/unit_cell_scale, resolution)
+    elif unit_cell_range == False:
+        ks = np.linspace(0, 2*np.pi/unit_cell_scale, resolution)
 
-	k_ret = []
-	Es = []
+    k_ret = []
+    Es = []
 
-	for k in ks:
-    	A = Harper_SOTI(p = 50, q = qk, zu = k)
-    	E = eigs_harper(A)
-    	Es.extend(E)
-    	k_ret.extend([k]*4*qk)
+    for k in ks:
+        A = Harper_SOTI(p = 50, q = qk, zu = k)
+        E = eigs_harper(A)
+        Es.extend(E)
+        k_ret.extend([k]*4*qk)
     
-	return k_ret, Es
+    return k_ret, Es
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def spectrum_plots():
-    # makes k vs energy plots
+# thin out arrays to plot
+def thin_arrays(phi, eps, thin_size):
     
+    indices = np.random.randint(len(phi)-1, size = int(thin_size))
 
+    phi_thin = np.asarray([phi[index] for index in indices])
+    eps_thin = np.asarray([eps[index] for index in indices])
+    
+    return phi_thin, eps_thin
 
+# sums over all k and plots the resulting phi vs eps pattern
+def sum_over_k(dimless_param, thin_size = 1e6, resolution = 100, unit_cell_scale = 1, 
+    Thin = False):
 
+    ks = np.linspace(-np.pi/unit_cell_scale,np.pi/unit_cell_scale,resolution)
+    
+    phi_ks = []
+    eps_ks = []
+    
+    if dimless_param == 1:
+        for k in ks:
+            phi, eps = main(mu = k)
+            phi_ks.extend(phi)
+            eps_ks.extend(eps)
+            
+    elif dimless_param == 2:
+        for k in ks:
+            phi, eps = main(nu = k)
+            phi_ks.extend(phi)
+            eps_ks.extend(eps)
+            
+    elif dimless_param == 3:
+        for k in ks:
+            phi, eps = main(zu = k)
+            phi_ks.extend(phi)
+            eps_ks.extend(eps)
 
+    if thin == True:
+        phi_ks_thin, eps_ks_thin = thin_arrays(phi_ks, eps_ks, thin_size)
+        return phi_ks_thin, eps_ks_thin
 
+    elif thin == False:
+        return phi_ks, eps_ks
+
+# gets Es for given ks
+def get_ke_spectrum(dimless_param, p, q, mu = 0, nu = 0, zu = 0, 
+    resolution = 100,unit_cell_scale=1):
+    Es = []
+    k_ret = []
+    ks = np.linspace(-np.pi/unit_cell_scale,np.pi/unit_cell_scale,resolution)
+
+    # get energies for ks
+    for k in ks:
+        q = int(q)
+        
+        if dimless_param == 1:
+            H = Harper_SOTI(p = p, q = q, mu = k, nu = nu, zu = zu)
+        elif dimless_param == 2:
+            H = Harper_SOTI(p = p, q = q, mu = mu, nu = k, zu = zu)
+        elif dimless_param == 3:
+            H = Harper_SOTI(p = p, q = q, mu = mu, nu = nu, zu = k)
+
+        eigs_k = eigs_harper(H)
+        Es.extend(eigs_k)
+        k_ret.extend([k]*4*q)
+
+    return k_ret, Es
+
+futura = {'fontname':'Futura'}
+
+# makes k vs e plots for given phis
+def spectrum_plots(dimless_param,  mu = 0, nu = 0, zu = 0,
+    ps = [0,1,10,25,37,50], q = 100, resolution = 100, unit_cell_scale = 1):
+    # ps must be a len six 1d array
+
+    fig, ax = plt.subplots(nrows = 2, ncols = int(len(ps)/2), figsize = (20,10), 
+        sharey = True, sharex = True)
+    fig.subplots_adjust(wspace=0.1,hspace=0.25)
+    
+    for i in range(len(ps)):
+        p_k = ps[i]
+        ks, Es = get_ke_spectrum(dimless_param = dimless_param, mu = mu, nu = nu, zu = zu, 
+            p = p_k, q = q, resolution = resolution, unit_cell_scale = unit_cell_scale)
+
+        # fix indexing
+        if i < 3:
+            j = 0
+        elif i >= 3:
+            j = 1
+            i -= 3
+        
+        # set labels
+        if i == 0:
+            ax[j][i].set_ylabel(r"${}$ - momentum".format(dimless_param), fontsize = 15, **futura)
+        if j == 1: 
+            ax[j][i].set_xlabel(r"Energy $\epsilon$", fontsize = 15, **futura)
+        
+        # plot
+        ax[j][i].scatter(Es,ks,c='k',marker='.',s=1)
+        #ax[j][i].set_xlim([-4,4]) # <- commented out for generality
+        ax[j][i].set_title(r"$\Phi = {:.2}$".format(p_k/q), **futura)
 
 
 
